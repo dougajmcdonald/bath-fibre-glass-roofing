7980debfc78801ce1ec2d3c0dc2838e195865af0
Doug McDonald <dougajmcdonald@gmail.com>
2020-12-04 19:07:11 +0000

    casing

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = () => {
       style={{ minHeight: `70px` }}
     >
       <div className="w-4/5 md:w-2/3 mx-auto py-4 flex flex-wrap items-center justify-between">
-        <h1 className="text-l text-gray-100 my-0">Bath fibre glass roofing</h1>
+        <h1 className="text-l text-gray-100 my-0">Bath Fibre Glass Roofing</h1>
         <nav className="md:block">
           <Button to={"/"}>Call us now 07966 232134</Button>
         </nav>
