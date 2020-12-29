2f5e67fa38178149183ece6d1e6ca8f4656b2acf
Doug McDonald <dougajmcdonald@gmail.com>
2020-12-29 20:07:55 +0000

    jiggle some vert wiggles

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,13 +4,13 @@ import Checkmark from "../svg/checkmark-outline.svg"
 import Button from "../components/button"
 
 const Hero = () => (
-  <section className="bg-white pt-16">
+  <section className="bg-white pt-12">
     <div className="w-4/5 md:w-2/3 mx-auto flex flex-wrap md:justify-between pt-16 pb-8 md:py-16">
       <div className="w-full md:w-2/3 md:pr-10">
         <h2 className="text-gray-800 font-bold text-xl md:text-3xl md:mr-20 font-body mt-0 border-primary border-b-4 w-32">
           Welcome
         </h2>
-        <p className="text-l md:text-xl">
+        <p className="text-l md:text-xl my-8">
           We have been working with fibreglass roofing systems since 1995 and
           carry out all types of roofing.
         </p>
